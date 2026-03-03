7fccf005da0cfbfc22b8eb4c5168bae1b11ee61c
Y Shivani <shivaniyenugundla@gmail.com>
2026-03-03 09:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (89.71%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -3,15 +3,15 @@ class Solution:
         if n == 1:
             return '0'
         
-        mid = 2**(n-1)  # middle index in 1-indexed string
+        mid = 2**(n-1)  
         
         if k == mid:
             return '1'
         elif k < mid:
-            # First half: recurse on Sn-1
+           
             return self.findKthBit(n-1, k)
         else:
-            # Second half: mirrored position in first half, then invert
+            
             mirror_pos = 2**n - k
             bit = self.findKthBit(n-1, mirror_pos)
             return '1' if bit == '0' else '0'
