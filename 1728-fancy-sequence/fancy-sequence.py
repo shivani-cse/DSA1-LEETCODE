3301146e6b80f42715f6a12d288dfd2412d07e21
Y Shivani <shivaniyenugundla@gmail.com>
2026-03-15 09:14:47 +0530

    Time: 225 ms (51.61%) | Memory: 59.8 MB (12.90%) - LeetSync

--- 1728-fancy-sequence/fancy-sequence.py
+++ 1728-fancy-sequence/fancy-sequence.py
@@ -3,10 +3,9 @@ class Fancy:
     
     def __init__(self):
         self.seq = []
-        self.mul = 1  # cumulative multiplier
-        self.add = 0  # cumulative addition
-        self.history = []  # store (val, mul_at_append, add_at_append)
-    
+        self.mul = 1  
+        self.add = 0  
+        self.history = []  
     def append(self, val: int) -> None:
         # store value with current state
         self.history.append((val, self.mul, self.add))
@@ -23,8 +22,8 @@ class Fancy:
             return -1
         
         val, mul0, add0 = self.history[idx]
-        # modular inverse of mul0
+       
         inv_mul0 = pow(mul0, self.MOD - 2, self.MOD)
-        # apply lazy updates
+       
         res = (val * self.mul * inv_mul0 + (self.add - add0 * self.mul * inv_mul0) % self.MOD) % self.MOD
         return res
