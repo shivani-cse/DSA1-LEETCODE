038201b3c40cac4685692e1498141f8f7e02a6af
Y Shivani <shivaniyenugundla@gmail.com>
2026-03-18 19:47:12 +0530

    Time: 8 ms (80.67%) | Memory: 25.1 MB (83.09%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m * n != len(original):
+            return []
+        result=[]
+        for i in range(m):
+            row = original[i*n:(i+1)*n]    
+            result.append(row)
+        return result    
+        
