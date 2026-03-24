77f47bdf1a95c88230227d99d7377195a5b47e35
Y Shivani <shivaniyenugundla@gmail.com>
2026-03-24 15:10:18 +0530

    Time: 1 ms (91.99%) | Memory: 20.3 MB (88.80%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,22 @@
+from collections import defaultdict, deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph=defaultdict(list)
+        indegree=[0]*numCourses
+        for course,dest in prerequisites:
+            graph[dest].append(course)
+            indegree[course]+=1
+        q=deque([])
+        for i in range(numCourses):
+            if indegree[i]==0:
+                q.append(i)
+        finish=0
+        while q:
+            node=q.popleft()
+            finish+=1
+            for nei in graph[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        return finish==numCourses                         
+        
