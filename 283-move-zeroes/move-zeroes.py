5abe94d68ad91309ab6c0632c92ccacacf351b37
Y Shivani <shivaniyenugundla@gmail.com>
2026-07-19 18:54:52 +0530

    Time: 7 ms (40.55%) | Memory: 20.6 MB (23.87%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,7 +1,5 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-
-       #Do not return anything, modify nums in-place instead
        slow=0
        for fast in range(len(nums)):
         if nums[fast]!=0:
